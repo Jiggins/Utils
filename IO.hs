c612555cf07b8a797c197ef9b93a8d33f070aa22
Jiggins <jackhiggins07@gmail.com>
2015-02-20 14:50:11 +0000

    Documentation, fixes and removed a monadic bind

--- IO.hs
+++ IO.hs
@@ -1,10 +1,12 @@
 module IO where
 
+-- | Print each element of a list on a new line
 printLines :: (Show a) => [a] -> IO()
 printLines = mapM_ print
 
+-- | Reads from STDIN, returns a list of Strings seperated by '\n'
 getLines :: IO [String]
-getLines = getContents >>= return . lines
+getLines = fmap lines getContents
 
 getNumbers :: (Num a, Read a) => IO [a]
 getNumbers = getContents >>= return . map read . lines
@@ -24,11 +26,13 @@ hackerrankListNum = getLine >> getNumbers
 hackerrank :: IO a -> IO a
 hackerrank = (>>) getLine
 
+-- | Takes input from STDIN until a blank line is entered.  Returns an IO list
 terminal :: IO [String]
 terminal = f []
   where f xs = getLine >>= \x -> if x == ""
-                                   then return xs
+                                   then return $ reverse xs
                                    else f (x:xs)
 
-terminalWith :: Show a => (String -> a) -> IO ()
-terminalWith function = terminal >>= mapM_ print . map function
+-- | Takes input from STDIN until a blank line is entered.  Returns an IO list with the function applied
+terminalWith :: Show a => (String -> a) -> IO [a]
+terminalWith function = fmap (map function) terminal
